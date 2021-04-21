7a6807fc08b432b82a72e77116d9c197c52bded1
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2021-04-21 13:29:42 +0200

    Created Banking Controller with end to end testing.

---
+++ MicroRabbit.Banking.API/Controllers/BankingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly ILogger<BankingController> _logger;
+        private readonly IAccountService _accountService;
+
+        public BankingController(ILogger<BankingController> logger, IAccountService accountService)
+        {
+            _logger = logger;
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
