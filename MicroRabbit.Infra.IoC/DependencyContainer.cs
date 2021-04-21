7965a0dec66bf785306c5a697432d8a80271f5c7
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2021-04-21 08:58:41 +0200

    Initial commit for Dependency injection.

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain bus
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
